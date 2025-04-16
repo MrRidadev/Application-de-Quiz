c72ae065d7c46b2e317c35a40387cc7b109c1304
ridaDev <kiratterida@gmail.com>
2025-04-16 16:18:51 +0100

    créer component categorie et home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CategorieComponent } from "./categorie/categorie.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CategorieComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CategorieComponent } from './categorie/categorie.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component:HomeComponent
+    },
+    {
+        path: 'categorie',
+        component:CategorieComponent
+    }
+];
