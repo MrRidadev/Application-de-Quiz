4584321c86770ac802adb7038f8571496ad935f8
ridaDev <kiratterida@gmail.com>
2025-04-16 11:47:35 +0100

    créer component Accueil

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,3 +6,9 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
+
+
+
+
+
+
